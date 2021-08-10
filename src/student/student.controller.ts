d6c0e7eab5ce955c8eaa249606a0c0f5372cf7aa
Alegarciy <ale.garciy@gmail.com>
2021-08-09 19:31:30 -0600

    Creating students route (GET)

---
+++ src/student/student.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('/students')
+export class StudentController {
+  @Get()
+  getStudents() {
+    return 'All Students';
+  }
+}
+
+('/students');
